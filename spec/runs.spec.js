053cec6f18776432335b6352621ac2c552bcb4f0
Antony Donovan <antony.donovan@generalassemb.ly>
2016-07-14 08:53:30 -0400

    Add tests for solution (and solution).
    One commit on master.

--- spec/runs.spec.js
+++ spec/runs.spec.js
@@ -24,26 +24,26 @@ describe('runs', function () {
     });
   });
 
-  xdescribe('total distance', function () {
+  describe('total distance', function () {
 
     it('has the correct length', function () {
-      expect(runs.totalDistance()).to.equal(2600);
+      expect(user.totalDistance()).to.equal(2600);
     });
 
   });
 
-  xdescribe('longest run', function () {
+  describe('longest run', function () {
 
     it('has the correct length', function () {
-      expect(runs.longestRun()).to.equal(1400);
+      expect(user.longestRun()).to.equal(1400);
     });
 
   });
 
-  xdescribe('average speed', function () {
+  describe('average speed', function () {
 
     it('has the correct value', function () {
-      expect(runs.averageSpeed().toLocaleString('en-US')).to.equal('1.857');
+      expect(user.averageSpeed().toLocaleString('en-US')).to.equal('1.857');
     });
 
   });
